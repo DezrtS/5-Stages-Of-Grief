7ee729341d20ae2c9ca1484b216d74441f31874c
Daniel Fattakhov <20839461+coolcom300@users.noreply.github.com>
2024-02-13 06:17:33 -0500

    hud stuff
    added better hud art created recharging bar for ranged attack

--- Grief/Assets/Scripts/Systems/UI/HUDBarController.cs
+++ Grief/Assets/Scripts/Systems/UI/HUDBarController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
@@ -5,8 +6,8 @@ using UnityEngine;
 
 public class HUDBarController : MonoBehaviour
 {
-    [SerializeField] GameObject hitPointBar, abilityBar;
-    public float hitPointNum, abilityNum;
+    [SerializeField] GameObject hitPointBar, abilityBar, timerBar;
+    public float hitPointNum, abilityNum,timerbarNum;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class HUDBarController : MonoBehaviour
         //hitPointNum = PlayerController.Instance.Health;
         UpdateHPBar();
         abilityNum = 1;//if we decide to have multipule ability charges this will need to be changed to find the max ability uses
+        timerbarNum = 0.75f;
     }
 
     // Update is called once per frame
@@ -31,6 +33,7 @@ public class HUDBarController : MonoBehaviour
         if (IdAttack == "ice_shard")
         {
             abilityNum--;
+            timerbarNum = 0;
             //abilityBar.SetActive(false);
             Invoke("AbilityCanUse",0.75f);//this timer has to be set manualy this should be changed so that it can find the cooldown on its own
             //Debug.Log("is ice shard");
@@ -44,11 +47,19 @@ public class HUDBarController : MonoBehaviour
     private void UpdateHPBar()
     {
         hitPointNum = PlayerController.Instance.Health;
-        hitPointBar.gameObject.transform.localScale = new Vector3(hitPointNum,1,1);
+        hitPointBar.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(hitPointNum*50,50);
+        //    RectTransform.width = hitPointNum*50;
+        //hitPointBar.gameObject.transform.localScale = new Vector3(hitPointNum,1,1);
         //Debug.Log("hp=" + hitPointNum);
     }
     private void UpdateAbilityBar()
     {
         abilityBar.gameObject.transform.localScale = new Vector3(abilityNum, 1, 1);
+        if (abilityNum == 0)
+        {
+            timerbarNum = timerbarNum + Time.deltaTime;
+            timerbarNum = Mathf.Clamp(timerbarNum, 0, 0.75f);
+        }
+        timerBar.transform.localScale = new Vector3(timerbarNum, 1, 1);
     }
 }
